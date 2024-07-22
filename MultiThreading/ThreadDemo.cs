d0c1008d0e532d8591da3de57736d828247decd9
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-22 20:13:37 +0530

    Add project files.

---
+++ MultiThreading/ThreadDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    internal class ThreadDemo
+    {
+        static void test1()
+        {
+            for (int i = 1; i <= 100; i++)
+            
+                Console.WriteLine("Test 1 "+ i);
+            
+        }
+
+        static void test2()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("Test 2 " + i);
+                if (i == 50)
+                {
+                    Console.WriteLine("Main thread going to sleep");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Main thread Woke up");
+                }
+                   
+            }
+        }
+        static void test3()
+        {
+            for (int i = 1; i <= 100; i++)
+            
+                Console.WriteLine("Test 3 " + i);
+            
+        }
+        static void Main()
+        {
+            test1();
+            test2();
+            test3();
+        }
+    }
+}
